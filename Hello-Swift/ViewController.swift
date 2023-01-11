7d91d95bff2d9ab0a2be790473c023f5ea267117
Sergey Selyakov <sergey.selyakov@gmail.com>
2023-01-11 13:21:30 +0300

    Initial Commit

---
+++ Hello-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello-Swift
+//
+//  Created by Sergey Selyakov on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
